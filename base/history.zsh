7539d616e63572456e287f373e56830eec31168a
noriah <vix@noriah.dev>
2019-12-26 16:45:13 -0800

    Things and stuff

--- base/history.zsh
+++ base/history.zsh
@@ -9,4 +9,4 @@ setopt hist_ignore_dups
 setopt hist_ignore_space
 setopt hist_verify
 setopt inc_append_history
-setopt share_history
+#setopt share_history
--- plugins/osx/osx.plugin.zsh
+++ plugins/osx/osx.plugin.zsh
@@ -6,3 +6,21 @@ function vncviewer() {
 function rmdsstore() {
 	find "${@:-.}" -type f -name .DS_Store -delete
 }
+
+OSX_CAPTURE_FOLDER="$HOME/Desktop"
+
+function cap() {
+  screencapture "${OSX_CAPTURE_FOLDER}/capture-$(date +%Y%m%d_%H%M%S).png"
+}
+
+function capi() {
+  screencapture -i "${OSX_CAPTURE_FOLDER}/capture-$(date +%Y%m%d_%H%M%S).png"
+}
+
+function capiw() {
+  screencapture -i -w "${OSX_CAPTURE_FOLDER}/capture-$(date +%Y%m%d_%H%M%S).png"
+}
+
+alias capc="screencapture -c"
+alias capic="screencapture -i -c"
+alias capiwc="screencapture -i -w -c"
