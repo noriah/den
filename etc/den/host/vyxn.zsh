1982be909b350afb16f5fe502c6a0994eef37546
noriah <vix@noriah.dev>
2021-10-02 13:16:04 -0700

    remove gcp from paths

--- etc/den/host/vyxn.zsh
+++ etc/den/host/vyxn.zsh
@@ -1,7 +1,19 @@
-burrow homebrew
-burrow battery
+# vyxn is a MacBook running macOS
+
+# Include OSX Specific Things
 burrow osx
-burrow gcp
+
+# We have a battery
+burrow battery
+
+# We have homebrew installed
+burrow homebrew
+
+# GCP SDK
+# burrow gcp
+
+# SSH Specific things
 burrow ssh
 
+# Set our prompt icon color
 export POWERLEVEL9K_OS_ICON_FOREGROUND=201
--- usr/burrow/plans/gcp.zsh
+++ usr/burrow/plans/gcp.zsh
@@ -1,1 +1,6 @@
+# GCP
+env_default 'GCP_SDK' "$HOME_OPT/google-cloud-sdk"
+
+export PATH="$PATH:$GCP_SDK/bin"
+
 [ -d "$GCP_SDK" ] && . "$GCP_SDK/completion.zsh.inc"
