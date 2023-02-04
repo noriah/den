364e350f402223d0954a0251519987989240cf74
noriah <vix@noriah.dev>
2023-02-03 17:14:03 -0800

    new year new stuff. idk. leave me alone. im tired.

---
+++ bin/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/zsh
+
+if [ $# -eq 0 ]; then
+  exit 1
+fi
+
+file=$(basename "$1")
+
+if [ ! -f "$1" ]; then
+  exit 1
+fi
+
+if [ -f "/usr/local/bin/$file" ]; then
+  read -q "REPLY?overwrite? (y/n)"
+  echo
+  if [ $REPLY != y ]; then
+    exit 1
+  fi
+fi
+
+echo "copying $1 to /usr/local/bin"
+
+cp -f "$1" "/usr/local/bin/$file"
