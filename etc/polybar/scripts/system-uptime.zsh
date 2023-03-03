96ba6ef091fa45bed227de82703a8051ac31666d
noriah <vix@noriah.dev>
2023-03-03 00:19:09 -0800

    change uptime output

--- etc/polybar/scripts/system-uptime.zsh
+++ etc/polybar/scripts/system-uptime.zsh
@@ -1,3 +1,5 @@
 #!/usr/bin/env zsh
 
-uptime | awk '/up/ { d=$3; t=$5; sub(",", "", t) } END { print d "d " t "t" }'
+uptime | awk '
+  /up/ { d=$3; t=$5; sub(",", "", t); sub (":", "h ", t) }
+  END { print d "d " t "m" }'
