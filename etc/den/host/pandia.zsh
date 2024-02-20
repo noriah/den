e7f20a8bc77161dc2621beae924ed8b2185cfae2
noriah <vix@noriah.dev>
2024-02-19 23:32:38 -0800

    pandia

---
+++ etc/den/host/pandia.zsh
@@ -0,0 +1,17 @@
+# pandia is a server running Linux
+den::conf linux.zsh
+
+den::host::set::color 75
+
+# burrow plugin vim_mode
+
+# GCP SDK
+# GCP_SDK=""
+# burrow plugin gcloud
+
+# SSH Specific things
+burrow plugin ssh
+
+burrow plugin tmux
+
+# export POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=164
